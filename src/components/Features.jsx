2bb1272b988525a77e92fb674ff5a45e1e7ca57a
rohit yadav <rohit.yadav@kushfintech.com>
2024-12-23 14:54:02 +0545

    end of features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,36 @@
 /* eslint-disable react/prop-types */
+import { useRef, useState } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+const BentoTilt = ({ children, className = "" }) => {
+  const [transformStyle, setTransfromStyle] = useState("");
+  const itemRef = useRef();
+  const handleMouseMove = (e) => {
+    if (!itemRef.current) return;
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect();
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top) / height;
+    const tiltX = (relativeX - 0.5) * 5;
+    const tiltY = (relativeY - 0.5) * -5;
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98) `;
+    setTransfromStyle(newTransform);
+  };
+  const handleMouseLeave = () => {
+    setTransfromStyle("");
+  };
+
+  return (
+    <div
+      className={className}
+      ref={itemRef}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
+      {children}
+    </div>
+  );
+};
 const BentoCard = ({ src, title, description }) => {
   return (
     <div className="relative size-full">
@@ -36,7 +68,7 @@ const Features = () => {
           </p>
         </div>
 
-        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+        <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
           <BentoCard
             src="videos/feature-1.mp4"
             title={
@@ -47,9 +79,9 @@ const Features = () => {
             description="A cross-platform metagame app,turning your
         activities across web2 and web3 games into a rewarding adventure."
           />
-        </div>
+        </BentoTilt>
         <div className="grid h-[135vh] grid-cols-2 grid-row-3 gap-7">
-          <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+          <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
             <BentoCard
               src="videos/feature-2.mp4"
               title={
@@ -59,8 +91,8 @@ const Features = () => {
               }
               description="An anime and gaming-inspired NFT collection - the IP primed for expansion"
             />
-          </div>
-          <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
             <BentoCard
               src="videos/feature-3.mp4"
               title={
@@ -70,8 +102,8 @@ const Features = () => {
               }
               description="A gamified social hub, adding a new dimension of play to social interaction for web3 communities."
             />
-          </div>
-          <div className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+          </BentoTilt>
+          <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
             <BentoCard
               src="videos/feature-4.mp4"
               title={
@@ -80,6 +112,23 @@ const Features = () => {
                 </>
               }
               description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
+            />
+          </BentoTilt>
+          <div className="bento-tilt_2">
+            <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+              <h1 className="bento-title special-title max-w-64 text-black">
+                M<b>o</b>re c<b>o</b>ming s<b>o</b>on!
+              </h1>
+              <TiLocationArrow className="m-5 scale-[5] self-end" />
+            </div>
+          </div>
+          <div className="bento-tilt_2">
+            <video
+              src="videos/feature-5.mp4"
+              loop
+              muted
+              autoPlay
+              className="size-full object-cover object-center"
             />
           </div>
         </div>
